f61eec2e96fd960da433964436e3b8d9d6cf1f71
Thyago-Monnerat <thy_thyago2.5@hotmail.com>
2025-02-18 08:00:21 -0300

    feat: add token generation and validation in TokenService

--- src/main/java/com/auth/microservice/services/TokenService.java
+++ src/main/java/com/auth/microservice/services/TokenService.java
@@ -1,7 +1,16 @@
 package com.auth.microservice.services;
 
+import com.auth.microservice.dtos.UserLoginDTO;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 
 @Service
 public class TokenService {
@@ -9,5 +18,35 @@ public class TokenService {
     @Value("${security.key}")
     private String key;
 
+    private final Algorithm algorithm = Algorithm.HMAC256(key);
 
+    private final String issuer = "UserAuth";
+
+    private Instant generateInstant() {
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
+
+    public String generateToken(UserLoginDTO userLoginDTO) {
+        try {
+            return JWT.create()
+                    .withIssuer(issuer)
+                    .withSubject(userLoginDTO.username())
+                    .withExpiresAt(generateInstant())
+                    .sign(algorithm);
+        } catch (JWTCreationException e) {
+            throw new JWTCreationException(e.getMessage(), e);
+        }
+    }
+
+    public String validateToken(String token){
+        try {
+            return JWT.require(algorithm)
+                    .withIssuer(issuer)
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        }catch (JWTVerificationException e){
+            throw new JWTVerificationException(e.getMessage(), e);
+        }
+    }
 }
